14dddbd6748698e03b2d68e6efbf7b51dfa7abd7
linhletd <linhletd@gmail.com>
2020-10-27 14:50:07 +0700

    refactor

--- websocket/client.js
+++ websocket/client.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {connect, Provider} from 'react-redux';
-import {BrowserRouter, Switch, Route, Redirect, NavLink, useHistory, useLocation, useParams, useRouteMatch, withRouter} from 'react-router-dom';
+import {BrowserRouter, Switch, Route,withRouter} from 'react-router-dom';
 import store from './front/redux/store.js';
 import AuthLayout from './front/layouts/auth_layout.js';
 import UnauthLayout from './front/layouts/unauth_layout.js'
-window.addEventListener('offline', ()=>{
-    console.log('offline', Date.now())
-})
-window.addEventListener('online', ()=>{
-    console.log('online', Date.now())
-})
 class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleLoginEvent();
+    }
     handleLoginEvent = (data) =>{
         let handle = (userData) =>{
-            this.props.updateStore({type: 'LOGIN', data: userData});
             let ws = new window.WebSocket('ws://localhost:8080');
-            this.props.updateStore({type: 'OPENSOCKET', data: ws});
-            this.props.history.replace('/');
+            this.props.updateStore({type: 'LOGIN', data: {user: userData, socket: ws}});
+            let intentURL = sessionStorage.getItem('inTentURL');
+            sessionStorage.removeItem('inTentURL');
+            this.props.history.replace(intentURL || '/');
         }
         if(!data){
             let bc = new BroadcastChannel('bc1');
@@ -31,18 +30,14 @@ class App extends React.Component{
         }
 
     }
-    login = this.handleLoginEvent();
     render(){
         return (
             <div id = 'app'>
-                {/* <Switch>
-                    <Route path = '/auth'>
-                        <UnauthLayout {...this.props} handleLogin = {this.handleLoginEvent}/>
-                    </Route>
-                    <Route path = '/'> */}
-                        <AuthLayout/>
-                    {/* </Route>
-                </Switch> */}
+                {
+                    this.props.user ?
+                    <AuthLayout/> :
+                    <UnauthLayout handleLogin = {this.handleLoginEvent}/>
+                }
             </div>
         )
     }
@@ -54,7 +49,12 @@ function mapDispatchToProps(dispatch){
         }
     }
 }
-const RoutedApp = withRouter(connect(null, mapDispatchToProps)(App));
+function mapStateToProps(state){
+    return {
+        user: state.main.user
+    }
+}
+const RoutedApp = withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
 document.addEventListener('DOMContentLoaded',()=>{
     render(<BrowserRouter><Provider store = {store}><RoutedApp/></Provider></BrowserRouter>, document.getElementById('root'))
 })
