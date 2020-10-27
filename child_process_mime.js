e98b2c3f683a1fd08c2df363b343867d321506ed
linhletd <linhletd@gmail.com>
2020-10-27 15:56:36 +0700

    delete redundants

--- child_process_mime.js
+++
@@ -1,4 +0,0 @@
-let exec = require('child_process').exec;
-exec(`file --brief --mime server.js`, (err, mime) => {
-    console.log(mime)
-})
--- favicon_ico_req.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-http.createServer((req, res) => {
-    if(req.url === '/favicon.ico'){
-        res.writeHead(200, {'Content-Type': "image/x-icon"});
-        return res.end()
-    }
-    /* response for other routes*/
-})
