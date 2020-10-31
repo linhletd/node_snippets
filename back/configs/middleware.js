0dc07bcf07f326dc666c07e733e22e497ae6d139
linhletd <linhletd@gmail.com>
2020-10-31 22:17:10 +0700

    add setinitcookie and ensureinit cookie, change module to class style

--- back/configs/middleware.js
+++ back/configs/middleware.js
@@ -1,36 +1,45 @@
-const { session } = require("passport");
-
-module.exports = function(app){
-    return {
-        ensureAuthenticated(req, res, next){
-            let {sessionMap} = app;
-            let sockets = app.sessionMap.get(req.sessionID);
-            if(req.user){
-                sockets ? sockets.forEach(sock => {
-                    sock.expires = Date.now() + 14*24*3600*1000;
-                }) : "";
-                return next()
-            }
-            sockets ? (sockets.forEach(cur =>{
-                cur.close(4000, 'session terminated')
-            })) : "";
-            if(/^\/auth/.test(req.url)){
-                console.log(true)
-                return next();
-            }
-            res.clearCookie('InVzZXIi');
-            res.clearCookie('connect.sid');
-            let intent = Buffer.from(req.url).toString('base64');
-                res.cookie('aW50ZW50VVJM', intent, {httpOnly: false, sameSite: 'strict', maxAge: 100});
-            return res.redirect('/auth/login');
-        },
-        ensureSQLConnected(req, res, next){
-            if(app.conn){
-                return next();
-            }
-            else{
-                return res.json({err: 'Cannot connect to sql database'});
-            }
+const uuid = require('uuid');
+class CustomMiddleware{
+    constructor(app){
+        this.app = app;
+    }
+    ensureAuthenticated = (req, res, next) =>{
+        let sockets = this.app.sessionMap.get(req.sessionID);
+        if(req.user){
+            sockets ? sockets.forEach(sock => {
+                sock.expires = Date.now() + 14*24*3600*1000;
+            }) : "";
+            return next()
+        }
+        sockets ? (sockets.forEach(cur =>{
+            cur.close(4000, 'session terminated')
+        })) : "";
+        if(/^\/auth/.test(req.url)){
+            return next();
+        }
+        res.clearCookie('InVzZXIi');
+        res.clearCookie('connect.sid');
+        let intent = Buffer.from(req.url).toString('base64');
+            res.cookie('aW50ZW50VVJM', intent, {httpOnly: false, sameSite: 'strict', maxAge: 10000});
+        return res.redirect('/auth/login');
+    }
+    ensureSQLConnected = (req, res, next)=>{
+        if(this.app.conn){
+            return next();
+        }
+        else{
+            return res.json({err: 'Cannot connect to sql database'});
         }
     }
+    setInitialCookie(req, res, next){
+        res.cookie('init', uuid.v4(), {httpOnly: true, sameSite: 'strict', signed: true});
+        next();
+    }
+    ensureHavingInitCookie(req, res, next){
+        if(req.signedCookies.init){
+            return next();
+        }
+        res.redirect('/auth/login');
+    }
 }
+module.exports = CustomMiddleware;
