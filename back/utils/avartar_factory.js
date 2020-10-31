49f62a0481158dcaa8812eab740ae1215f1e6dea
linhletd <linhletd@gmail.com>
2020-10-31 22:29:57 +0700

    change gravatar source from http to https

--- back/utils/avartar_factory.js
+++ back/utils/avartar_factory.js
@@ -3,5 +3,5 @@ module.exports = function assignIcon(mail){
     let d = ['monsterid']//, 'identicon', 'wavartar', 'retro', 'robohash'];
     let randomIndex = Math.floor((d.length) * Math.random());
     let hash = crypto.Hash('md5').update(mail).digest('hex');
-    return `http://www.gravatar.com/avatar/${hash}?d=${d[randomIndex]}&s=45`
+    return `https://www.gravatar.com/avatar/${hash}?d=${d[randomIndex]}&s=45`
 }
