a4e7dac732a286edc4b98ca4ca55be783c628b18
linhletd <linhletd@gmail.com>
2020-10-31 22:54:51 +0700

    add id prop

--- front/layouts/sub_user_layout.js
+++ front/layouts/sub_user_layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const SubUserLayout = () =>{
     return(
-        <div>
+        <div id = 'user_layout'>
             <p>This content is not available right now, it will come soon...</p>
         </div>
     )
