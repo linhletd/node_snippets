fa241aff47047a328411c63129170e5c5739a9e5
linhletd <linhletd@gmail.com>
2020-10-31 22:27:03 +0700

    add initcookie middleware, change midd func call to constructor call

--- back/routes/unauth_routes.js
+++ back/routes/unauth_routes.js
@@ -1,17 +1,19 @@
 const router = require('express').Router();
 const handlersf = require('../apis/handle_unath_routes.js');
 const dotenv = require('dotenv').config();
-
+let Middleware = require('../configs/middleware');
 module.exports = function(app){
     let handlers = handlersf(app);
-    router.post('/auth/register', handlers.register);
-    router.post('/auth/verify/:type/:token', handlers.verifyToken);
+    let customMiddleware = new Middleware(app);
+    router.post('/auth/register',customMiddleware.ensureHavingInitCookie, handlers.register);
+    router.get('/auth/verify', handlers.verifyToken);
+    router.post('/auth/invoke-token', customMiddleware.ensureHavingInitCookie, handlers.invokeToken)
 
     router.post('/auth/login', handlers.login('local'));
     router.get('/auth/fb', handlers.thirdPartyAuth('facebook'));
     router.get('/auth/fb/callback',handlers.login('facebook'));
     router.get('/auth/github', handlers.thirdPartyAuth('github'))
     router.get('/auth/github/callback',handlers.login('github'));
-
+    
     app.use(router)
 }
