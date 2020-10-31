4dd5bc63068f18410d1174c552adb258656f37cb
linhletd <linhletd@gmail.com>
2020-10-31 23:18:49 +0700

    rename to avatar_factory

--- back/utils/avartar_factory.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-module.exports = function assignIcon(mail){
-    let d = ['monsterid']//, 'identicon', 'wavartar', 'retro', 'robohash'];
-    let randomIndex = Math.floor((d.length) * Math.random());
-    let hash = crypto.Hash('md5').update(mail).digest('hex');
-    return `https://www.gravatar.com/avatar/${hash}?d=${d[randomIndex]}&s=45`
-}
